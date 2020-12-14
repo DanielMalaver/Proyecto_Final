1a7483585b1933fcf3ac21c0f415c183204edbeb
Daniel_Malaver <malaverdaniel22@gmail.com>
2020-12-14 17:20:28 -0400

    Final sin detalles

--- Comandos.py
+++ Comandos.py
@@ -16,13 +16,23 @@ from Control_luces import*
 ####################################################
 # --------- Funciones del protocolo MQTT -----------
 ####################################################
-
-def PRUEBA(objeto):
-    print(objeto)
+SALIDA = {'Control':0}
 
 def conectado(client, userdata, flags, rc):
     print("CONECTADO A EL AULA Codigo de respuesta: "+str(rc))
-    cliente.subscribe("Aula")
+    cliente.subscribe("Horario")
+
+def conectadoPantalla(client, userdata, flags, rc):
+    print("Conectado a Pantalla de control Codigo de respuesta: "+str(rc))
+    cliente.subscribe("Pantalla_de_control")
+
+def mensajeMQTT_Pantalla(client, userdata, msg):
+    recibido = msg.payload.decode()
+    if recibido == "S" or recibido == "s":
+        SALIDA['Control'] = "S"
+    else:
+        SALIDA['Control'] = "N"
+    client.disconnect()
 
 # ------------ Conexión del cliente -----------------
 #------------ Datos del broker MQTT -----------------
@@ -33,6 +43,7 @@ puerto = 5050
 #------------ CICLO -----------------
 
 while(True):
+#------------ Imprime horario inicial a modo de comprobacion -----------------
     print("SALON AAAAA")
     print(Aula_A_Lunes)
     print(Aula_A_Martes)
@@ -51,34 +62,52 @@ while(True):
     print(Aula_C_Miercoles)
     print(Aula_C_Jueves)
     print(Aula_C_Viernes)
-# --------------------- ESpera de comando ------------------------
+# --------------------- Solicita y recibe comando ------------------------
+    Pantalla = "Ingrese un comando o ingrese la letra x para continuar:\n"
+    cliente = paho.Client()
+    cliente.connect(broker,puerto)
+    mensaje = cliente.publish("Pantalla_de_control",Pantalla)
     cliente = mqtt.Client()
     cliente.connect(broker,puerto)
     cliente.on_connect = conectado
-    cliente.on_message = mensajeMQTT_AULA
+    cliente.on_message = mensajeMQTT_Horario
     cliente.loop_forever()
 # -----------------------------------------------------------------
-    print("SALON AAAAA")
+#------------ Imprime horariomodificado a modo de comprobacion -----------------
+    print("SALON A")
     print(Aula_A_Lunes)
     print(Aula_A_Martes)
     print(Aula_A_Miercoles)
     print(Aula_A_Jueves)
     print(Aula_A_Viernes)
-    print("SALON BBBB")
+    print("SALON B")
     print(Aula_B_Lunes)
     print(Aula_B_Martes)
     print(Aula_B_Miercoles)
     print(Aula_B_Jueves)
     print(Aula_B_Viernes)
-    print("SALON CCCC")
+    print("SALON C")
     print(Aula_C_Lunes)
     print(Aula_C_Martes)
     print(Aula_C_Miercoles)
     print(Aula_C_Jueves)
     print(Aula_C_Viernes)
-    print("SALIMOS DE FINALLLLLLLLLLLLLLLLLL")
-    c =input("¿Quieres seguir modificando? [S/s] ")
-    if c == "S" or c == "s":
+
+# --------- Mostrar en Pantalla de control ---------------------
+    Pantalla = "¿Quieres continuar en la pantalla de control?"
+    cliente = paho.Client()
+    cliente.connect(broker,puerto)
+    mensaje = cliente.publish("Pantalla_de_control",Pantalla)
+#---------------------------------------------------------------
+# ----------Ingresar respuesta ---------------------------------
+    cliente = mqtt.Client()
+    cliente.connect(broker,puerto)
+    cliente.on_connect = conectadoPantalla
+    cliente.on_message = mensajeMQTT_Pantalla
+    cliente.loop_forever()
+# ------------------------------------------------------------------
+# ------ Comienza a correr el cronometro y el control de luces ------------------------------------
+    if SALIDA['Control'] == "S" or SALIDA['Control'] == "s":
         continue
     Control_Luces(Aula_A_Lunes,Aula_A_Martes,Aula_A_Miercoles,Aula_A_Jueves,Aula_A_Viernes,
         Aula_B_Lunes,Aula_B_Martes,Aula_B_Miercoles,Aula_B_Jueves,Aula_B_Viernes,
